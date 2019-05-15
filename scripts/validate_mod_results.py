053db248b76da3a5b15667722567c2f3bae58cc0
Marcus Stoiber <marcus.stoiber@nanoporetech.com>
2019-05-15 11:45:18 -0700

    Update mods validation script and added option for control sample.

--- scripts/validate_mod_results.py
+++ scripts/validate_mod_results.py
@@ -3,6 +3,10 @@ import sys
 import argparse
 import numpy as np
 import pandas as pd
+
+import matplotlib
+if sys.platform == 'darwin':
+    matplotlib.use("TkAgg")
 import seaborn as sns
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
@@ -21,8 +25,11 @@ VERBOSE = False
 parser = argparse.ArgumentParser()
 parser.add_argument(
     'megalodon_results_dir',
-    help='Output directory from basecall_and_more.py script ' +
-    '(run with mappings and per_read_mods outputs).')
+    help='Output directory from megalodon with mappings and per_read_mods ' +
+    'in outputs. Must have --write-mods-text set for mods validation.')
+parser.add_argument(
+    '--control-megalodon-results-dir',
+    help='Megalodon output directory with modified base control sample.')
 parser.add_argument(
     '--ground-truth-data',
     help='Ground truth csv with (chrm, pos, is_mod) values.')
@@ -31,57 +38,18 @@ parser.add_argument(
     help='String prefix for all mapped chromosomes with ground ' +
     'truth modifications. All other sites will be assumed unmodified.')
 parser.add_argument(
-    '--out-pdf', default='mod_base_refactor_validation.pdf',
+    '--out-pdf', default='megalodon_validation.pdf',
     help='Output pdf filename.')
 parser.add_argument(
-    '--verbose', action='store_true',
-    help='Output progress information on top of performance metrics.')
+    '--out-filename',
+    help='Output filename for text summary. Default: stdout')
+parser.add_argument(
+    '--quiet', action='store_true',
+    help='Suppress progress information.')
 
 
-def main():
-    args = parser.parse_args()
-    global VERBOSE
-    VERBOSE = args.verbose
-
-    if VERBOSE: print('Reading ground truth data')
-    gt_dat, mod_chrm_sw = None, None
-    if args.ground_truth_data is not None:
-        gt_dat = pd.read_csv(
-            args.ground_truth_data, header=None,
-            names=['chrm', 'pos', 'is_mod'])
-    elif args.mod_chrms_startswith is not None:
-        mod_chrm_sw = args.mod_chrms_startswith
-    else:
-        sys.stderr.write('ERROR: Must provide grounf truth.\n')
-        sys.exit(1)
-
-    if VERBOSE: print('Reading megalodon data')
-    mega_dat = pd.read_csv(
-        os.path.join(args.megalodon_results_dir, MODS_FN), sep='\t', header=None,
-        names=['read_id' ,'chrm', 'strand', 'pos', 'score',
-               'motif', 'mod_base'])
-    mega_dat['chrm'] = mega_dat['chrm'].str.replace('humanGRCh38_', '')
-    try:
-        bc_dat = pd.read_csv(
-            os.path.join(args.megalodon_results_dir, MAP_FN), sep='\t')
-        mean_bc_acc = np.mean(bc_dat['pct_identity'])
-        med_bc_acc = np.median(bc_dat['pct_identity'])
-    except FileNotFoundError:
-        mean_bc_acc, med_bc_acc = 0, 0
-        if VERBOSE: print('Basecalls not found')
-
-    if VERBOSE: print('Merging ground truth information')
-    # merge scores with known mod sites
-    if gt_dat is not None:
-        m_dat = pd.merge(mega_dat, gt_dat, on=['chrm', 'pos'], sort=False)
-    else:
-        m_dat = mega_dat
-        m_dat['is_mod'] = np.array([
-            chrm.startswith(mod_chrm_sw) for chrm in m_dat['chrm']])
-
-    if VERBOSE: print('Modified base class distribution:\n' +
-                      str(m_dat['is_mod'].value_counts()))
-    if VERBOSE: print('Computing PR/ROC')
+def report_mod_metrics(m_dat, args, out_fp):
+    if VERBOSE: sys.stderr.write('Computing PR/ROC\n')
     # compute roc and presicion recall
     precision, recall, _ = precision_recall_curve(
         m_dat['is_mod'], -m_dat['score'])
@@ -90,11 +58,11 @@ def main():
     fpr, tpr, _ = roc_curve(m_dat['is_mod'], -m_dat['score'])
     roc_auc = auc(fpr, tpr)
 
-    if VERBOSE: print('Metrics (bc_mean_acc, bc_med_acc, pr_auc, roc_auc)')
-    print('{}\t{:.4f}\t{:.4f}\t{:.6f}\t{:.6f}'.format(
-        args.megalodon_results_dir, mean_bc_acc, med_bc_acc, avg_prcn, roc_auc))
+    out_fp.write(('Modified base metrics:\n\t' +
+                  'Average precision:  {:.6f}\n\t' +
+                  'ROC AUC:            {:.6f}\n').format(avg_prcn, roc_auc))
 
-    if VERBOSE: print('Plotting')
+    if VERBOSE: sys.stderr.write('Plotting\n')
     with PdfPages(args.out_pdf) as pdf:
         bw = 0.2
         gs = 1000
@@ -128,6 +96,103 @@ def main():
         plt.title('ROC curve: auc={:0.2f}'.format(roc_auc))
         pdf.savefig(bbox_inches='tight')
         plt.close()
+
+    return
+
+def merge_mods_data(mega_dat, ctrl_dat, gt_dat, mod_chrm_sw, out_fp):
+    if VERBOSE: sys.stderr.write('Merging modified base data\n')
+    # merge scores with known mod sites
+    if ctrl_dat is not None:
+        mega_dat['is_mod'] = np.full(mega_dat.shape[0], True)
+        ctrl_dat['is_mod'] = np.full(ctrl_dat.shape[0], False)
+        m_dat = mega_dat.append(ctrl_dat)
+    elif gt_dat is not None:
+        m_dat = pd.merge(mega_dat, gt_dat, on=['chrm', 'pos'], sort=False)
+    else:
+        m_dat = mega_dat
+        m_dat['is_mod'] = np.array([
+            chrm.startswith(mod_chrm_sw) for chrm in m_dat['chrm']])
+    out_fp.write(('Modified base class distribution:\n\tNumber of ' +
+                  'modified observations:    {}\n\tNumber of ' +
+                  'unmodified observations:  {}\n').format(
+                      sum(m_dat['is_mod']), sum(~m_dat['is_mod'])))
+
+    return m_dat
+
+def report_acc_metrics(res_dir, out_fp):
+    try:
+        bc_dat = pd.read_csv(
+            os.path.join(res_dir, MAP_FN), sep='\t')
+        mean_bc_acc = np.mean(bc_dat['pct_identity'])
+        med_bc_acc = np.median(bc_dat['pct_identity'])
+        # crude mode by rounding to 1 decimal
+        uniq_acc, acc_counts = np.unique(np.around(
+            bc_dat['pct_identity'], 1), return_counts=True)
+        mode_bc_acc = uniq_acc[np.argmax(acc_counts)]
+        out_fp.write(
+            ('Basecall metrics for {}:\n\t' +
+             'Mean Pct. Identity:    {:.4f}\n\t' +
+             'Median Pct. Identity:  {:.4f}\n\t' +
+             'Mode Pct. Identity:    {:.1f}\n').format(
+                 res_dir, mean_bc_acc, med_bc_acc, mode_bc_acc))
+    except FileNotFoundError:
+        if VERBOSE: sys.stderr.write(
+                '*' * 20 + 'Mappings not found for {}\n'.format(res_dir))
+
+    return
+
+def parse_mega_data(args, out_fp):
+    if VERBOSE: sys.stderr.write('Reading megalodon data\n')
+    mega_dat = pd.read_csv(
+        os.path.join(args.megalodon_results_dir, MODS_FN),
+        sep='\t', header=None,
+        names=['read_id' ,'chrm', 'strand', 'pos', 'score',
+               'motif', 'mod_base'])
+
+    report_acc_metrics(args.megalodon_results_dir, out_fp)
+
+    return mega_dat
+
+def parse_control_mods(args, out_fp):
+    ctrl_dat = gt_dat = mod_chrm_sw = None
+    if args.control_megalodon_results_dir is not None:
+        if VERBOSE: sys.stderr.write('Reading control mods data\n')
+        ctrl_dat = pd.read_csv(
+            os.path.join(args.control_megalodon_results_dir, MODS_FN),
+            sep='\t', header=None,
+            names=['read_id' ,'chrm', 'strand', 'pos', 'score',
+                   'motif', 'mod_base'])
+        report_acc_metrics(args.control_megalodon_results_dir, out_fp)
+    elif args.ground_truth_data is not None:
+        if VERBOSE: sys.stderr.write('Reading ground truth data\n')
+        gt_dat = pd.read_csv(
+            args.ground_truth_data, header=None,
+            names=['chrm', 'pos', 'is_mod'])
+    elif args.mod_chrms_startswith is not None:
+        mod_chrm_sw = args.mod_chrms_startswith
+    else:
+        sys.stderr.write(
+            '*' * 20 + 'WARNING: No modified base ground truth provided.\n')
+
+    return ctrl_dat, gt_dat, mod_chrm_sw
+
+def main():
+    args = parser.parse_args()
+    global VERBOSE
+    VERBOSE = not args.quiet
+    out_fp = sys.stdout if args.out_filename is None else \
+             open(args.out_filename, 'w')
+
+    mega_dat = parse_mega_data(args, out_fp)
+
+    ctrl_dat, gt_dat, mod_chrm_sw = parse_control_mods(args, out_fp)
+    # could just compute mapping metrics
+    if all(d is None for d in (ctrl_dat, gt_dat, mod_chrm_sw)):
+        if out_fp is not sys.stdout: out_fp.close()
+        return
+    m_dat = merge_mods_data(mega_dat, ctrl_dat, gt_dat, mod_chrm_sw, out_fp)
+    report_mod_metrics(m_dat, args, out_fp)
+    if out_fp is not sys.stdout: out_fp.close()
 
     return
 
