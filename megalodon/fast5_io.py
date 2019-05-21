87ef041061940fd193e3f3f917980acc7213f384
Marcus Stoiber <marcus.stoiber@nanoporetech.com>
2019-05-21 11:04:17 -0700

    Converted fast5 file listing to enumeration so that reads are added to processing queue before full directory is searched for files.

--- megalodon/fast5_io.py
+++ megalodon/fast5_io.py
@@ -1,10 +1,21 @@
 import os
+from glob import glob
 
 from ont_fast5_api.fast5_interface import get_fast5_file
-from ont_fast5_api.conversion_tools.conversion_utils import get_fast5_file_list
 
 from megalodon import megalodon_helper as mh
 
+
+def iterate_fast5_filenames(input_path, recursive):
+    if recursive:
+        for root, _, fns in os.walk(input_path, followlinks=True):
+            for fn in fns:
+                if not fn.endswith('.fast5'): continue
+                yield os.path.join(root, fn)
+    else:
+        for fn in glob(os.path.join(input_path, '*.fast5')):
+            yield fn
+    return
 
 def iterate_fast5_reads(fast5s_dir, limit=None, recursive=True):
     """Return iterator yielding reads in a directory of fast5 files or a
@@ -18,7 +29,7 @@ def iterate_fast5_reads(fast5s_dir, limit=None, recursive=True):
     :param recursive: Search path recursively for fast5 files.
     """
     nreads = 0
-    for fast5_fn in get_fast5_file_list(fast5s_dir, recursive=recursive):
+    for fast5_fn in iterate_fast5_filenames(fast5s_dir, recursive):
         with get_fast5_file(fast5_fn, 'r') as fast5_fp:
             for read_id in fast5_fp.get_read_ids():
                 yield fast5_fn, read_id
