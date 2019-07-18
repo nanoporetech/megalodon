aed50f5a5de4d393035718f1ba956372ae01472e
Marcus Stoiber <marcus.stoiber@nanoporetech.com>
2019-07-18 13:51:39 -0700

    Remove name from bedmethyl output as it adds no information.

--- megalodon/mods.py
+++ megalodon/mods.py
@@ -586,12 +586,11 @@ class ModBedMethylWriter(object):
                 continue
             cov = mod_site.get_coverage()
             self.handles[mod_base].write(
-                ('{chrom}\t{pos}\t{end}\t{name}\t{score}\t{strand}\t{pos}' +
+                ('{chrom}\t{pos}\t{end}\t.\t{score}\t{strand}\t{pos}' +
                  '\t{end}\t0,0,0\t{cov}\t{perc}\n').format(
                      chrom=mod_site.chrom, pos=mod_site.pos,
-                     end=mod_site.pos + 1, name=mod_site.id,
-                     strand=mod_site.strand, cov=cov, score=min(int(cov), 1000),
-                     perc=int(mod_prop * 100)))
+                     end=mod_site.pos + 1, strand=mod_site.strand, cov=cov,
+                     score=min(int(cov), 1000), perc=int(mod_prop * 100)))
             self.handles[mod_base].flush()
 
         return
