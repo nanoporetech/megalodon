cc14f2674bf60a1a478dda6ecb14d4b968e762fb
Marcus Stoiber <marcus.stoiber@nanoporetech.com>
2020-06-11 14:29:52 -0700

    Convert modified base wiggle output reverse strand to negative values.

--- megalodon/mods.py
+++ megalodon/mods.py
@@ -1887,13 +1887,15 @@ class ModWigWriter(object):
     """
     def __init__(
             self, basename, mods, mode='w',
-            strands={'+': 'fwd_strand', '-': 'rev_strand'}):
+            strands={'+': 'fwd_strand', '-': 'rev_strand'},
+            strand_factor={'+': 1, '-': -1}):
         self.basename = basename
         self.mods = mods
         self.mods_lookup = dict(mods)
         self.mod_short_names, self.mod_long_names = zip(*self.mods)
         self.mode = mode
         self.strands = strands
+        self.strand_factor = strand_factor
 
         self.mod_sites_data = dict(
             ((mod_short_name, strand), defaultdict(list))
@@ -1910,7 +1912,9 @@ class ModWigWriter(object):
                            'wiggle output.')
                 continue
             self.mod_sites_data[(mod_base, mod_site.strand)][
-                mod_site.chrom].append((mod_site.pos, mod_prop))
+                mod_site.chrom].append((
+                    mod_site.pos,
+                    mod_prop * self.strand_factor[mod_site.strand]))
 
     def close(self):
         # write all data on close since all data is required to write
